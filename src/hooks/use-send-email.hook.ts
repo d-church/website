a0655df347ba6ad3d66d621c8a2db1669fd95aa9
Danylo <danilaka76@outlook.com>
2024-07-10 14:09:11 +0300

    Email service now integrated with emailjs.

--- src/hooks/use-send-email.hook.ts
+++ src/hooks/use-send-email.hook.ts
@@ -6,9 +6,19 @@ import { MainPageService } from "@/service/main-page.service";
 import { apiUrls } from "@/utils/apiUrl";
 
 export function useSendEmail() {
+  const credentials = {
+    service_id: process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+    template_id: process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+    user_id: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_API_KEY!,
+    template_params: {},
+  };
+
   const { isPending, mutate, isSuccess, isError } = useMutation({
-    mutationKey: [apiUrls.sendEmail],
-    mutationFn: (data: TSendMessage) => axios.post(apiUrls.sendEmail, data),
+    mutationKey: [apiUrls.sendEmailJS],
+    mutationFn: (data: TSendMessage) => {
+      credentials.template_params = { ...data };
+      return axios.post(apiUrls.sendEmailJS, credentials);
+    },
   });
 
   return { isPending, mutate, isSuccess, isError };
