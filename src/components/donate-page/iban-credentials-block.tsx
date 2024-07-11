36cc8985017bde22f1ef637fd13aca4d253a5d57
Danylo <danilaka76@outlook.com>
2024-07-11 17:54:26 +0300

    Iban credentials was changed.

--- src/components/donate-page/iban-credentials-block.tsx
+++ src/components/donate-page/iban-credentials-block.tsx
@@ -3,13 +3,16 @@ import { useTranslations } from "next-intl";
 export function IbanCredentialsBlock() {
   const t = useTranslations();
   return (
-    <div className="3xl:scale-150 m-auto max-2xl:scale-90">
+    <div className="m-auto max-2xl:scale-90 3xl:scale-150">
       <p className="mb-[10px] mt-[50px] text-center text-xl/[1.5rem] font-bold">
         {t("donate-page.other-donate-methods.credentials.header")}
       </p>
       <div className="max-w-[850px] text-center text-lg/[1.75rem] font-medium lg:text-xl/[1.875rem]">
-        <p className="whitespace-normal uppercase md:whitespace-pre-wrap">
+        <p className="hidden whitespace-pre-wrap uppercase md:block">
           {t("donate-page.other-donate-methods.credentials.inline")}
+        </p>
+        <p className="whitespace-pre-wrap uppercase md:hidden">
+          {t("donate-page.other-donate-methods.credentials.inline-mobile")}
         </p>
       </div>
     </div>
