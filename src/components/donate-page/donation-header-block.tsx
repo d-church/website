331ed1e7cc8fba7cd696ab192a22dabcd67d61ce
Danylo <danilaka76@outlook.com>
2024-07-11 18:29:36 +0300

    Donate page was optimised for mobiles.

--- src/components/donate-page/donation-header-block.tsx
+++ src/components/donate-page/donation-header-block.tsx
@@ -3,7 +3,7 @@ import { useTranslations } from "next-intl";
 export function DonationHeaderBlock() {
   const t = useTranslations();
   return (
-    <div className="3xl:scale-150 3xl:mt-[5rem] mt-[20px] text-center text-lg/[1.75rem] font-medium max-2xl:scale-90 xl:mt-0">
+    <div className="mt-[20px] text-center text-sm font-medium max-2xl:scale-90 md:text-lg/[1.75rem] xl:mt-0 3xl:mt-[5rem] 3xl:scale-150">
       <p className="mb-[10px] font-bold uppercase">
         {t("donate-page.preview-block.button-donate")}
       </p>
