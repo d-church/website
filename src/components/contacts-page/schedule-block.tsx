0167b34ad6468fedef44273238a571ed4553ff19
Danylo <danilaka76@outlook.com>
2024-07-17 13:47:45 +0300

    Text data on the contacts page has been changed.

--- src/components/contacts-page/schedule-block.tsx
+++ src/components/contacts-page/schedule-block.tsx
@@ -11,20 +11,22 @@ export function ScheduleBlock() {
   const t = useTranslations("contacts-page");
 
   return (
-    <div className="container flex flex-col items-center justify-center gap-[50px] pb-[53px] pt-[40px] text-center lg:flex-row lg:justify-around lg:text-start xl:gap-[360px] 2xl:items-baseline 2xl:justify-normal">
-      <div className="w-[328px] text-3xl 2xl:ml-[274px]">
+    <div className="container flex flex-col items-center justify-center gap-[50px] pb-[53px] pt-[40px] text-center lg:flex-row lg:justify-around lg:text-start xl:gap-[295px] 2xl:items-baseline 2xl:justify-normal">
+      <div className="w-fit text-3xl 2xl:ml-[274px]">
         <p className="mb-[20px]">{t("schedule.header")}</p>
-        <div className="flex w-full items-end gap-[10px] border-b border-gray">
-          <p className="font-bold">{t("schedule.morning.time")}</p>
-          <p className="text-xl/[1.5rem]">{t("schedule.morning.text")}</p>
-        </div>
-        <div className="flex w-full items-end gap-[10px] border-b border-gray">
-          <p className="font-bold">{t("schedule.day.time")}</p>
-          <p className="text-xl/[1.5rem]">{t("schedule.day.text")}</p>
-        </div>
-        <div className=" flex w-full items-end gap-[10px]">
-          <p className="font-bold">{t("schedule.evening.time")}</p>
-          <p className="text-xl/[1.5rem]">{t("schedule.evening.text")}</p>
+        <div className="w-[328px]">
+          <div className="flex w-full items-end gap-[10px] border-b border-gray">
+            <p className="font-bold">{t("schedule.morning.time")}</p>
+            <p className="text-xl/[1.5rem]">{t("schedule.morning.text")}</p>
+          </div>
+          <div className="flex w-full items-end gap-[10px] border-b border-gray">
+            <p className="font-bold">{t("schedule.day.time")}</p>
+            <p className="text-xl/[1.5rem]">{t("schedule.day.text")}</p>
+          </div>
+          <div className=" flex w-full items-end gap-[10px]">
+            <p className="font-bold">{t("schedule.evening.time")}</p>
+            <p className="text-xl/[1.5rem]">{t("schedule.evening.text")}</p>
+          </div>
         </div>
       </div>
       <div className="mt-[10px] w-[335px] text-[1.25rem]/[1.625rem] font-medium">
