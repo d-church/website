543a5bc507003230d8b6b7550a063420e51fdaf7
Artem Zhurbei <zhurbeyartem@gmail.com>
2024-09-16 17:03:00 +0300

    fix: hover on img

--- src/components/events-and-blog-page/blog-block.tsx
+++ src/components/events-and-blog-page/blog-block.tsx
@@ -5,6 +5,7 @@ import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
 
 import { Link } from "@/app/navigation";
+import s from './blog-block.module.css'
 
 interface IBlogBlockProps {
   title: string;
@@ -18,24 +19,24 @@ export function BlogBlock({ title, date, imgSrc, id }: IBlogBlockProps) {
   const t = useTranslations("events-and-blogs-page.blogs");
   return (
     <div className="font-medium">
-      <Link href={href}>
+      <Link href={href} className={s.link}>
         <Image
           src={imgSrc}
           alt="Blog preview image"
-          className="rounded-[20px] max-2xl:h-[160px] max-2xl:w-[320px] max-xl:rounded-[12px] 2xl:h-[300px] 2xl:w-[520px] "
+          className={`rounded-[20px] max-2xl:h-[160px] max-2xl:w-[320px] max-xl:rounded-[12px] 2xl:h-[300px] 2xl:w-[520px]`}
           width={320}
           height={160}
         />
       </Link>
-      <div>
-        <div className="flex justify-between gap-[20px] pt-[10px] xl:gap-[30px] xl:pt-[20px]">
+      <div className={s.container}>
+        <div className={`flex justify-between gap-[20px] pt-[10px] xl:gap-[30px] xl:pt-[20px] ${s.btnContainer}`}>
           <Link href={href} className="peer">
             <p className="xl:text-[1rem]/[1.21rem] 2xl:text-[1.375rem]/[1.67rem] ">
               {title}
             </p>
           </Link>
           <Button
-            className="max-2lx:rounded-[20px] group flex items-center justify-center rounded-[22px] border border-graphite bg-white px-[10px] py-[10px] text-[1rem] uppercase text-black peer-hover:bg-graphite peer-hover:text-hover-blue hover:bg-graphite hover:text-hover-blue 2xl:px-[20px] 2xl:text-[1.25rem]"
+            className={`max-2lx:rounded-[20px] group flex items-center justify-center rounded-[22px] border border-graphite bg-white px-[10px] py-[10px] text-[1rem] uppercase text-black peer-hover:bg-graphite peer-hover:text-hover-blue hover:bg-graphite hover:text-hover-blue 2xl:px-[20px] 2xl:text-[1.25rem] ${s.btn}`}
             asChild
           >
             <Link href={href}>{t("button")}</Link>
