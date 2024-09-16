9254192d988b7ab18ea3c6a57f9615f1031e3335
Artem Zhurbei <zhurbeyartem@gmail.com>
2024-09-16 16:47:32 +0300

    fix: add hover on text

--- src/components/events-and-blog-page/blog-block.tsx
+++ src/components/events-and-blog-page/blog-block.tsx
@@ -13,7 +13,7 @@ interface IBlogBlockProps {
   id: number;
 }
 
-export function BlogBlock({ title, date, imgSrc, id }: IBlogBlockProps) {  
+export function BlogBlock({ title, date, imgSrc, id }: IBlogBlockProps) {
   const href = `/events-and-blog/${id}`;
   const t = useTranslations("events-and-blogs-page.blogs");
   return (
@@ -22,20 +22,20 @@ export function BlogBlock({ title, date, imgSrc, id }: IBlogBlockProps) {
         <Image
           src={imgSrc}
           alt="Blog preview image"
-          className="rounded-[20px] max-2xl:h-[160px] max-2xl:w-[320px] max-xl:rounded-[12px] 2xl:h-[300px] 2xl:w-[520px]"
+          className="rounded-[20px] max-2xl:h-[160px] max-2xl:w-[320px] max-xl:rounded-[12px] 2xl:h-[300px] 2xl:w-[520px] "
           width={320}
           height={160}
         />
       </Link>
       <div>
         <div className="flex justify-between gap-[20px] pt-[10px] xl:gap-[30px] xl:pt-[20px]">
-          <Link href={href}>
-            <p className="xl:text-[1rem]/[1.21rem] 2xl:text-[1.375rem]/[1.67rem]">
+          <Link href={href} className="peer">
+            <p className="xl:text-[1rem]/[1.21rem] 2xl:text-[1.375rem]/[1.67rem] ">
               {title}
             </p>
           </Link>
           <Button
-            className="max-2lx:rounded-[20px] group flex items-center justify-center rounded-[22px] border border-graphite bg-white px-[10px] py-[10px] text-[1rem] uppercase text-black hover:bg-graphite hover:text-hover-blue 2xl:px-[20px] 2xl:text-[1.25rem]"
+            className="max-2lx:rounded-[20px] group flex items-center justify-center rounded-[22px] border border-graphite bg-white px-[10px] py-[10px] text-[1rem] uppercase text-black peer-hover:bg-graphite peer-hover:text-hover-blue hover:bg-graphite hover:text-hover-blue 2xl:px-[20px] 2xl:text-[1.25rem]"
             asChild
           >
             <Link href={href}>{t("button")}</Link>
